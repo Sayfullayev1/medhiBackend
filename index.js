bcfeb43aaf41e5538221b513aeb981b9d2b1915d
SayfullayevSirojiddin <-email@example.com>
2024-11-07 05:25:53 +0500

    commit

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(express.json());
 
 
 
-console.log("",);
+// const tok = jwt.sign({ username: 'user1' }, process.env.JWT_SECRET);
+
+// console.log(tok);
 
 
 
@@ -42,13 +44,194 @@ const authenticateToken = ((req, res, next) => {
 });
 
 // Пример защищенного маршрута для получения данных
-app.get('/api/data', authenticateToken, (req, res) => {
+app.get('/schedule/B24T01', authenticateToken, (req, res) => {
   const data = {
-    message: 'Данные успешно получены',
-    users: [
-      { username: 'user111', age: 25 },
-      { username: 'user2', age: 30 },
-    ],
+
+    groupName: 'Б24-Т03 (141)  13',
+
+    schedule: [
+        {
+            dayOfTheWeek: "Понедельник",
+            data: "04.11.2024",
+            lessons: [
+                {
+                    lessonTime: '9:00-10:20',
+                    lessonTitle: 'Иностранный язык (английский)',
+                    teachersName: 'Рахимова С.Д.',
+                    rumNumber: '220',
+                },
+                {
+                    lessonTime: '10:30-11:50',
+                    lessonTitle: 'Иностранный язык (английский)',
+                    teachersName: 'Рахимова С.Д.',
+                    rumNumber: '220',
+                },
+                {
+                    lessonTime: '12:40-14:00',
+                    lessonTitle: 'Общая физика (механика) (лаб.раб.)',
+                    teachersName: 'Шовдирова С.Л.',
+                    rumNumber: '234',
+                },
+                {
+                    lessonTime: '14:10-15:30',
+                    lessonTitle: 'Общая физика (механика) (лаб.раб.)',
+                    teachersName: 'Шовдирова С.Л.',
+                    rumNumber: '234',
+                },
+            ],
+        },
+        {
+            dayOfTheWeek: "Вторник",
+            data: "05.11.2024",
+            lessons: [
+                {
+                    lessonTime: '9:00-10:20',
+                    lessonTitle: 'Maтематический анализ (лекция)',
+                    teachersName: 'Рахматуллаев А.М.',
+                    rumNumber: '208',
+                },
+                {
+                    lessonTime: '10:30-11:50',
+                    lessonTitle: 'Математический анализ (пр)',
+                    teachersName: 'Рахматуллаев А.М.',
+                    rumNumber: '325',
+                },
+                {
+                    lessonTime: '12:40-14:00',
+                    lessonTitle: 'Иностранный язык (русский)',
+                    teachersName: 'Тошматова Т.Т',
+                    rumNumber: '207',
+                },
+                {
+                    lessonTime: '14:10-15:30',
+                    lessonTitle: 'Физическая культура',
+                    teachersName: ' Сияев С.',
+                    rumNumber: 'спзал',
+                },
+            ],
+        },
+        {
+            dayOfTheWeek: "Среда",
+            data: "06.11.2024",
+            lessons: [
+                {
+                    lessonTime: '9:00-10:20',
+                    lessonTitle: 'Аналитическая геометрия (лекция)',
+                    teachersName: 'Шарипов А.С.',
+                    rumNumber: '206',
+                },
+                {
+                    lessonTime: '10:30-11:50',
+                    lessonTitle: 'Информатика',
+                    teachersName: ' Егамбердиев Б.Б.',
+                    rumNumber: '210',
+                },
+                {
+                    lessonTime: '12:40-14:00',
+                    lessonTitle: 'Аналитическая геом. (практика)',
+                    teachersName: 'Шарипов А.С.',
+                    rumNumber: '206',
+                },
+                {
+                    lessonTime: '14:10-15:30',
+                    lessonTitle: 'Иностранный язык (английский)',
+                    teachersName: 'Рахимова С.Д.',
+                    rumNumber: '220',
+                },
+            ],
+        },
+        {
+            dayOfTheWeek: "Четверг",
+            data: "07.11.2024",
+            lessons: [
+                {
+                    lessonTime: '9:00-10:20',
+                    lessonTitle: '',
+                    teachersName: '',
+                    rumNumber: '',
+                },
+                {
+                    lessonTime: '10:30-11:50',
+                    lessonTitle: 'Общая физика (механика) (лек)',
+                    teachersName: 'Пяк П.Э.',
+                    rumNumber: '208',
+                },
+                {
+                    lessonTime: '12:40-14:00',
+                    lessonTitle: 'Общая физика (механика) (практика)',
+                    teachersName: 'Пяк П.Э.',
+                    rumNumber: '202',
+                },
+                {
+                    lessonTime: '14:10-15:30',
+                    lessonTitle: 'Физическая культура',
+                    teachersName: 'Сияев С.',
+                    rumNumber: 'спзал',
+                },
+            ],
+        },
+        {
+            dayOfTheWeek: "Пятница",
+            data: "08.11.2024",
+            lessons: [
+                {
+                    lessonTime: '9:00-10:20',
+                    lessonTitle: 'Математический анализ (лекция)',
+                    teachersName: 'Рахматуллаев А.М.',
+                    rumNumber: '102',
+                },
+                {
+                    lessonTime: '10:30-11:50',
+                    lessonTitle: 'Химия (лек) за 30.09.2024',
+                    teachersName: 'Мухтаров О.П.',
+                    rumNumber: '104',
+                },
+                {
+                    lessonTime: '12:40-14:00',
+                    lessonTitle: 'Урок духовности и просвещения',
+                    teachersName: 'Юлдашева Ш.С.',
+                    rumNumber: '102',
+                },
+                {
+                    lessonTime: '14:10-15:30',
+                    lessonTitle: 'Математический анализ (лекция)',
+                    teachersName: 'Рахматуллаев А.М.',
+                    rumNumber: '214',
+                },
+            ],
+        },
+        {
+            dayOfTheWeek: "Субота",
+            data: "09.11.2024",
+            lessons: [
+                {
+                    lessonTime: '9:00-10:20',
+                    lessonTitle: 'История России (практика)',
+                    teachersName: 'Алимджанов Б.А.',
+                    rumNumber: '214',
+                },
+                {
+                    lessonTime: '10:30-11:50',
+                    lessonTitle: 'Иностранный язык (русский)',
+                    teachersName: 'Тошматова Т.Т.',
+                    rumNumber: '207',
+                },
+                {
+                    lessonTime: '12:40-14:00',
+                    lessonTitle: 'Общая физика (механика) (практика)',
+                    teachersName: 'Пяк П.Э.',
+                    rumNumber: '325',
+                },
+                {
+                    lessonTime: '14:10-15:30',
+                    lessonTitle: 'Химия (лаб.раб.)',
+                    teachersName: 'Аширматова Н.',
+                    rumNumber: '119',
+                },
+            ],
+        },
+
+        ],
   };
   res.json(data); // Отправляем данные обратно клиенту
 });
